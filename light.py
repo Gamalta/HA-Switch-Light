e68648a72e423476622e2bf031479b87a5828b11
Gamalta <eliovital@icloud.com>
2025-06-13 20:25:35 +0200

    chore

--- light.py
+++ light.py
@@ -53,6 +53,7 @@ class DynamicControlledLight(LightEntity):
         self._attr_rgb_color = self._get_light_attr(ATTR_RGB_COLOR)
         self._attr_rgbw_color = self._get_light_attr(ATTR_RGBW_COLOR)
         self._attr_rgbww_color = self._get_light_attr(ATTR_RGBWW_COLOR)
+        _LOGGER.error('_attr_brightness %s', self._get_light_attr(ATTR_BRIGHTNESS))
         _LOGGER.error('_attr_supported_color_modes %s', self._get_light_attr(ATTR_SUPPORTED_COLOR_MODES))
         _LOGGER.error('_attr_supported_features %s', self._get_light_attr('supported_features'))
         self._attr_supported_color_modes = self._get_light_attr(ATTR_SUPPORTED_COLOR_MODES, [ColorMode.ONOFF])
